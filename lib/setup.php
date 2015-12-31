009c7d8f138d3b7fd14aa73821097f3fb35725e6
Alex Schmoe <aschmoe@gmail.com>
2015-12-30 21:56:40 -0800

    switching out sidebar logic for pages

--- lib/setup.php
+++ lib/setup.php
@@ -86,6 +86,7 @@ function display_sidebar() {
     is_404(),
     is_front_page(),
     is_page_template('template-custom.php'),
+    is_page(),
   ]);
 
   return apply_filters('proud/display_sidebar', $display);
