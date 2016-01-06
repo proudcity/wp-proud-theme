f513534bdb7d28e760b100afa39fea40fc8978e3
Alex Schmoe <aschmoe@gmail.com>
2016-01-06 13:03:38 -0800

    fixing up post display

--- templates/content-single.php
+++ templates/content-single.php
@@ -3,13 +3,20 @@
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
+      <p class="muted"><?php echo get_the_date(); ?></p>
+      <hr />
     </header>
     <div class="entry-content">
+      <?php if( has_post_thumbnail() ): ?>
+      <p>
+        <?php the_post_thumbnail(); ?>
+      </p>
+      <?php endif; ?>
       <?php the_content(); ?>
     </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'proud'), 'after' => '</p></nav>']); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    <?php //comments_template('/templates/comments.php'); ?>
   </article>
 <?php endwhile; ?>
