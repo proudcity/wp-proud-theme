adc626ad686645adbea26322f802c6fed980d165
Alex Schmoe <aschmoe@gmail.com>
2016-01-07 01:36:20 -0800

    removing teasers, adding filters

--- lib/setup.php
+++ lib/setup.php
@@ -44,6 +44,8 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
   add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
 
+  // Make visual default.
+  add_filter( 'wp_default_editor', create_function('', 'return "tinymce";') );
   // Use main stylesheet for visual editor
   // To add custom styles edit /assets/styles/layouts/_tinymce.scss
   add_editor_style(Assets\asset_path('styles/proud-vendor.css'));
