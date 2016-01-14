385e0c135c768e53c5e272dc1693cabb2197e7f4
Alex Schmoe <aschmoe@gmail.com>
2016-01-14 13:43:43 -0800

    fixing up templates

--- templates/content-single-agency.php
+++ templates/content-single-agency.php
@@ -1,3 +1,17 @@
-<div class="full-width-container"> 
-  <?php the_content(); ?>
-</div>
+<?php use Proud\Theme\Titles; ?>
+
+<article <?php post_class(); ?>>
+  <?php if( !Titles\titleHidden() ) : ?>
+  <header>
+    <h1 class="entry-title"><?php the_title(); ?></h1>
+  </header>
+  <?php endif; ?>
+  <div class="entry-content">
+      <?php if( has_post_thumbnail() && !Titles\titleHidden() ): ?>
+      <p>
+          <?php the_post_thumbnail(); ?>
+      </p>
+      <?php endif; ?>
+    <?php the_content(); ?>
+  </div>
+</article>
