a430beb4ce1d56f69b5c91ca928fac11cfe11cd5
Jeff Lyon <jeff@albatrossdigital.com>
2016-01-27 02:04:01 -0800

    Tweaking doc templates

--- templates/content-single-document.php
+++ templates/content-single-document.php
@@ -1,5 +1,14 @@
+<?php
+$id = get_the_ID();
+$src = get_post_meta( $id, 'document', true );
+$meta = json_decode(get_post_meta( $id, 'document_meta', true ));
+$terms = wp_get_post_terms( $id, 'document_taxonomy', array("fields" => "all"));
+?>
 <a href="/documents" class="btn btn-default btn-sm pull-right"><i class="fa fa-arrow-left"></i> Back to documents</a>
-<h1 class="entry-title"><?php the_title(); ?></h1>
+<h1 class="entry-title">
+  <?php the_title(); ?>
+  <small><?php foreach ($terms as $term): ?><a class="label label-default" href="?filter_categories[]=<?php echo $term->term_id ?>"><?php echo $term->name ?></a><?php endforeach; ?></small>
+</h1>
 <div class="row">
   <div class="col-md-3">
     <!--<p><img src="<?php echo $meta->icon; ?>" /></p>-->
@@ -14,5 +23,3 @@
     <iframe src="http://docs.google.com/gview?url=<?php echo urlencode($src); ?>&embedded=true" style="width:100%; height:500px;" frameborder="0"></iframe>
   </div>
 </div>
-
-
