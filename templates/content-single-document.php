486a2180ab1bf1b8c0d396056ca636969522c35a
Jeff Lyon <jeff@albatrossdigital.com>
2016-03-17 16:42:04 -0700

    Doc page cleanup #206

--- templates/content-single-document.php
+++ templates/content-single-document.php
@@ -6,22 +6,31 @@ $meta = json_decode(get_post_meta( $id, 'document_meta', true ));
 $terms = wp_get_post_terms( $id, 'document_taxonomy', array("fields" => "all"));
 $show_preview = $meta->filetype == 'pdf' && ( strpos(strtoupper($meta->size), 'KB') !== FALSE || ( strpos($meta->size, 'MB') !== FALSE && (int)str_replace(' MB', '', $meta->size) < 10 ) );
 ?>
-<a href="/documents" onclick="history.go(-1);return false;" class="btn btn-default btn-sm pull-md-right header-margin"><i class="fa fa-arrow-left"></i> Back to documents</a>
+<div class="page-header">
+  <h2><a href="/documents" onclick="history.go(-1);return false;">Documents</a></h2>
+</div>
+
 <h1 class="entry-title">
   <?php the_title(); ?>
-  <?php foreach ($terms as $term): ?><a class="label label-default" href="?filter_categories[]=<?php echo $term->term_id ?>"><?php echo $term->name ?></a><?php endforeach; ?>
 </h1>
+
 <div class="row">
   <div class="col-md-3">
     <!--<p><img src="<?php echo $meta->icon; ?>" /></p>-->
-    <h5><?php the_date('M d Y'); ?></h5>
-    <ul class="list-inline list-inline-middot icon-list">
-      <li><?php echo strtoupper($meta->filetype); ?></li>
-      <li><?php echo $meta->size; ?></li>
-    </ul>
+    <p>
+      <?php foreach ($terms as $term): ?>
+        <a class="label label-default" href="?filter_categories[]=<?php echo $term->term_id ?>"><?php echo $term->name ?></a>
+      <?php endforeach; ?>
+    </p>
+    <p>
+      <div><?php the_date('F j Y'); ?></div>
+      <ul class="list-inline list-inline-middot icon-list">
+        <li><?php echo strtoupper($meta->filetype); ?></li>
+        <li><?php echo $meta->size; ?></li>
+      </ul>
+    </p>
     <?php if (!empty($src)): ?>
-      <p><a href="<?php echo $src; ?>" class="btn btn-primary btn-lg" download="<?php echo $filename; ?>"><i class="fa fa-download"></i> Download</a></p>
-      <p class="text-small text-muted"><?php echo $filename; ?></p>
+      <p><a href="<?php echo $src; ?>" class="btn btn-primary" download="<?php echo $filename; ?>"><i class="fa fa-download"></i> Download</a></p>
     <?php endif; ?>
   </div>
   <div class="col-md-9">
