1b0e62565b7961a186aea2e106282e47ffa6e1ef
Jeff Lyon <jeff@albatrossdigital.com>
2016-04-05 00:43:37 -0700

    Change payment template from JS redirect to full page that includes Gravityform if necessary https://github.com/proudcity/wp-proudcity/issues/261

--- templates/content-single-payment.php
+++ templates/content-single-payment.php
@@ -1,16 +1,41 @@
 <?php
-$js_path = esc_url('/payments') . '#/city/payments/' . $post->post_name;
-?>
+use Proud\Theme\Wrapper;
 
-<article <?php post_class(); ?>>
-  <header>
-    <h1 class="entry-title"><?php the_title(); ?></h1>
-  </header>
-  <div class="entry-content" id="entry-content">
-    <?php the_content(); ?>
+$id = get_the_ID();
+
+$form_id = get_post_meta( $id, 'form', true );
+if ( !empty($form_id) ) {
+
+  // Docs: https://www.gravityhelp.com/documentation/article/embedding-a-form/#usage-examples
+  // gravity_form( $id_or_title, $display_title = true, $display_description = true, $display_inactive = false, $field_values = null, $ajax = false, $tabindex, $echo = true );
+  $form = gravity_form( $form_id, false, true, false, null, false, 0, false );
+}
+else {
+  $link = $filename = get_post_meta( $id, 'link', true );
+}
+
+?>
+<div class="page-header">
+  <h2><a href="/payments" onclick="jQuery('.navbar-btn.payments-button').trigger('click');return false;">Payments</a></h2>
+</div>
+
+<h1 class="entry-title">
+  <?php the_title(); ?>
+</h1>
+
+
+<div class="row">
+  <div class="col-md-7">
+    <h4>Online payments</h4>
+    <?php if ($form): ?>
+      <?php echo $form ?>
+    <?php elseif ($link): ?>
+      <p><a href="<?php echo $link ?>"class="btn btn-primary btn-lg" target="_blank">Pay online now &raquo;</a></p>
+      <p><small><em>You will be redirected to our secure online payment provider.</em></small></p>
+    <?php endif ?>
   </div>
-</article>
-<script type="text/javascript">
-  jQuery('#entry-content').html('<div class="text-center"><i class="fa fa-spinner fa-pulse fa-4x"></i></div>');
-  window.location = '<?php print $js_path ?>';
-</script>
+  <div class="col-md-5">
+    <h4>Other ways to pay</h4>
+    <?php echo the_content() ?>
+  </div>
+</div>
