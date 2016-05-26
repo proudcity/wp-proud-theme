ab408018f60b548c032ed5885115db7549036770
Alex Schmoe <aschmoe@gmail.com>
2016-05-26 13:00:58 -0700

    removing viewport experiment, modiifying search appearance to be larger and centered

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,6 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, target-densityDpi=device-dpi">
+  <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
   <?php wp_head(); ?>
 </head>
