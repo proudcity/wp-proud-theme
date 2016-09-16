5ff3527af3d9d79418de3ed3510b5ffcc60be65b
Jeff Lyon <jeff@albatrossdigital.com>
2016-09-16 15:02:53 -0700

    Allow Person column to be hidden from Agency list widget https://github.com/proudcity/wp-proudcity/issues/511

--- templates/teaser-items/teaser-agency-table-header.php
+++ templates/teaser-items/teaser-agency-table-header.php
@@ -2,7 +2,7 @@
   <?php
   echo sprintf( '<thead><tr><th>%s</th><th>%s</th><th>%s</th><th>%s</th><th>%s</th></tr></thead>',    
     _x( 'Agency', 'post type singular name', 'wp-agency' ),   
-    __( 'Person', 'proud-agency' ),   
+    empty($this->hide['person']) ? __( 'Person', 'proud-agency' ) : '',   
     __( 'Phone', 'proud-teaser' ),    
     __( 'Contact', 'proud-teaser' ),    
     empty($this->hide['social']) ? __( 'Social', 'proud-teaser' ) : ''
--- templates/teaser-items/teaser-agency-table.php
+++ templates/teaser-items/teaser-agency-table.php
@@ -2,7 +2,7 @@
   
   <td><?php the_title( sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>' ); ?></td>
   
-  <td><?php echo !empty( $meta['name'][0] ) ? $meta['name'][0] : '' ?></td>
+  <td><?php echo empty($this->hide['person']) && !empty( $meta['name'][0] ) ? $meta['name'][0] : '' ?></td>
   
   <td><?php if( !empty( $meta['phone'][0] ) ): ?>
     <?php echo sprintf( '<a href="%s"><i class="fa fa-fw fa-phone"></i>%s</a>', esc_url( 'tel:' . $meta['phone'][0] ) , $meta['phone'][0] ); ?>
@@ -20,5 +20,5 @@
     <?php if( !empty( $meta['social_facebook'][0] ) ): ?><a href="<?php echo esc_url( $meta['social_facebook'][0] ) ?>"><i class="fa fa-fw fa-facebook-square"></i></a><?php endif; ?>
     <?php if( !empty( $meta['social_twitter'][0] ) ): ?><a href="<?php echo esc_url( $meta['social_twitter'][0] ) ?>"><i class="fa fa-fw fa-twitter-square"></i></a><?php endif; ?>
   </td>
-  
+
 </tr>
