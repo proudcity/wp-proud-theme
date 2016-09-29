815ad37ad1048975cedd63de72aac12436d06f05
Jeff Lyon <jeff@albatrossdigital.com>
2016-09-29 14:30:35 -0700

    Adding Answsers content list https://github.com/proudcity/wp-proudcity/issues/488

---
+++ templates/teaser-items/teaser-question-list-header.php
@@ -0,0 +1,10 @@
+<!-- 
+@todo: we are currently linking to the previous answer because the topnav takes up 50px.  
+We should revisit this when we figure it out: https://github.com/proudcity/wp-proudcity/issues/576.
+-->
+<a name="answer-list-legend"></a>
+<ul>
+  <?php foreach($this->query->posts as $key => $post): ?>
+    <li><?php print sprintf( '<a href="%s" rel="bookmark">%s</a>', '#answer-' . ($key == 0 ? 'list-legend' : $this->query->posts[$key-1]->ID), get_the_title($post)  ); ?></li>
+  <?php endforeach; ?>
+</ul>
---
+++ templates/teaser-items/teaser-question-list.php
@@ -0,0 +1,3 @@
+<a name="answer-<?php echo get_the_id(); ?>"></a>
+<h3><?php echo the_title(); ?> </h3>
+<?php echo the_content(); ?> 
