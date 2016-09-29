c88c429c27a7e03d573e6fc37995949ff8f13661
Jeff Lyon <jeff@albatrossdigital.com>
2016-09-29 14:42:32 -0700

    Adding missing opening div

--- templates/teaser-items/teaser-question-list-header.php
+++ templates/teaser-items/teaser-question-list-header.php
@@ -8,3 +8,4 @@ We should revisit this when we figure it out: https://github.com/proudcity/wp-pr
     <li><?php print sprintf( '<a href="%s" rel="bookmark">%s</a>', '#answer-' . ($key == 0 ? 'list-legend' : $this->query->posts[$key-1]->ID), get_the_title($post)  ); ?></li>
   <?php endforeach; ?>
 </ul>
+<div>
