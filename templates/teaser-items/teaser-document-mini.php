dfef4e5db4533661c4aa799a0ea9813ddc544514
Jeff Lyon <jeff@albatrossdigital.com>
2016-10-05 16:50:29 -0700

    Add mini document teaser list format https://github.com/proudcity/wp-proudcity/issues/589

---
+++ templates/teaser-items/teaser-document-mini.php
@@ -0,0 +1,23 @@
+<?php 
+use Proud\Document;
+$icon = Document\get_document_icon();
+$title = ($icon == 'fa-globe') ? 'title="Complete this form online"' : 'title="View/download form"';
+?>
+
+<div <?php post_class( "teaser" ); ?>>
+  <div class="row">
+    
+    <div class="col-xs-2 col-md-1">
+      <a href="<?php echo esc_url( get_permalink() ) ?>" <?php echo $title ?>>
+        <i class="fa fa-3x text-muted filetype-icon <?php echo $icon ?>"></i>
+      </a>
+    </div>
+    <div class="col-md-10">
+      <h4 class="entry-title" style="margin-top:0;">
+        <?php the_title( sprintf( '<a href="%s" rel="bookmark" %s>', esc_url( get_permalink() ), $title ), '</a>' ); ?>
+      </h4>
+      <div class="text-muted text-small"><?php echo get_the_date(); ?></div>
+    </div>
+
+  </div>
+</div>
