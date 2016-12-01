cade58bc0a50a4e453986051b62323824f790d1d
Alex Schmoe <aschmoe@gmail.com>
2016-12-01 15:36:39 -0800

    adding popup on question sub-page https://github.com/proudcity/wp-proudcity/issues/637

--- templates/content-single-question.php
+++ templates/content-single-question.php
@@ -1,7 +1,7 @@
 <?php
 $terms = wp_get_post_terms( get_the_ID(), 'faq-topic');
 $term_slug = !empty($terms) ?  $terms[0]->slug . '/' : '';
-$js_path = '/city/answers/' . $term_slug . $post->post_name;
+$js_path = '/' . $term_slug . $post->post_name;
 ?>
 
 <article <?php post_class(); ?>>
@@ -13,7 +13,9 @@ $js_path = '/city/answers/' . $term_slug . $post->post_name;
   </div>
 </article>
 <script type="text/javascript">
-  window.location.hash = '<?php echo $js_path; ?>'
-  //jQuery('#entry-content').html('<div class="text-center"><i class="fa fa-spinner fa-pulse fa-4x"></i></div>');
-  //window.location = '<?php print $js_path ?>';
+  (function($, Proud) {
+    $(document).ready(function() {
+      Proud.proudNav.triggerOverlay('answers', '<?php echo $js_path ?>');
+    });
+  })(jQuery, Proud);
 </script>
