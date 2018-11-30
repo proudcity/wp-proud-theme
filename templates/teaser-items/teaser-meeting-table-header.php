41123eb1b5004b4e7bbb7cc5941870a6c6933315
Jeff Lyon <jeff@albatrossdigital.com>
2018-11-29 18:34:12 -0800

    Meetings: keep file labels in column rather than sliding over https://github.com/proudcity/wp-proudcity/issues/1452

--- templates/teaser-items/teaser-meeting-table-header.php
+++ templates/teaser-items/teaser-meeting-table-header.php
@@ -1,6 +1,6 @@
 <div class="table-responsive"><table class="table table-striped">
   <?php
-  echo sprintf( '<thead><tr><th>%s</th><th>%s</th><th>%s</th></tr></thead>',
+  echo sprintf( '<thead><tr><th width="80%%">%s</th><th width="20%%">%s</th><th colspan="3" width="0">%s</th></tr></thead>',
     __( 'Name', 'proud-teaser' ),
     empty($this->hide['date']) ? __( 'Date', 'proud-teaser' ) : '',
     empty($this->hide['content_available']) ? __( 'Content', 'proud-teaser' ) : ''
