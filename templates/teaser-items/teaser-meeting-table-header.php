a8505e84616647c0a0b9e09efe2b786f5221a1df
Jeff Lyon <jeff@albatrossdigital.com>
2018-12-13 20:20:22 -0800

    Fix headers in meeting table https://github.com/proudcity/wp-proudcity/issues/1470

--- templates/teaser-items/teaser-meeting-table-header.php
+++ templates/teaser-items/teaser-meeting-table-header.php
@@ -1,8 +1,18 @@
 <div class="table-responsive"><table class="table table-striped">
   <?php
-  echo sprintf( '<thead><tr><th width="80%%">%s</th><th width="20%%">%s</th><th colspan="3" width="0">%s</th></tr></thead>',
+  if (empty($this->hide['date'])) {
+    $dateLabel =  __( 'Date', 'proud-teaser' );
+  }
+  elseif (empty($this->hide['time'])) {
+    $dateLabel =  __( 'Time', 'proud-teaser' );
+  }
+  else {
+    $dateLabel = '';
+  }
+
+  echo sprintf( '<thead><tr><th width="80%%">%s</th><th width="20%%">%s</th><th colspan="4" width="0">%s</th></tr></thead>',
     __( 'Name', 'proud-teaser' ),
-    empty($this->hide['date']) ? __( 'Date', 'proud-teaser' ) : '',
+    $dateLabel,
     empty($this->hide['content_available']) ? __( 'Content', 'proud-teaser' ) : ''
   );
   ?>
