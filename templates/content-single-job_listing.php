20d75e4b9e926a5a34f5a156eb955df02663c77b
Alex Schmoe <aschmoe@gmail.com>
2019-03-20 22:56:27 -0700

    fixing job application link https://github.com/proudcity/wp-proudcity/issues/1518

--- templates/content-single-job_listing.php
+++ templates/content-single-job_listing.php
@@ -8,6 +8,17 @@
     <div class="row">
       <div class="col-md-8">
         <?php the_content(); ?>
+        <?php if ( candidates_can_apply() ) : ?>
+            <?php
+              $apply = get_the_job_application_method();
+              if ( !empty( $apply->type) &&  ( $apply->type === 'url' ||  $apply->type === 'email' ) ) :
+                /**
+                 * job_manager_application_details_email or job_manager_application_details_url hook
+                 */
+                do_action( 'job_manager_application_details_' . $apply->type, $apply );
+			        ?>
+            <?php endif;//get_job_manager_template( 'job-application.php' ); ?>
+        <?php endif; ?>
       </div>
       <div class="col-md-4 right-sidebar">
         <?php dynamic_sidebar('sidebar-job'); ?>
