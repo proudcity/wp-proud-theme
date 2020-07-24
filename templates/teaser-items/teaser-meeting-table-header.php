fac70267be2f302d87396a692df54a3625cdfc0d
Jeff Lyon <jeff@albatrossdigital.com>
2020-07-23 21:26:56 -0700

    Meeting list table cuts off top right corner #1727

--- templates/teaser-items/teaser-meeting-table-header.php
+++ templates/teaser-items/teaser-meeting-table-header.php
@@ -10,7 +10,7 @@
     $dateLabel = '';
   }
 
-  echo sprintf( '<thead><tr><th width="80%%">%s</th><th width="20%%">%s</th><th colspan="4" width="0">%s</th></tr></thead>',
+  echo sprintf( '<thead><tr><th width="80%%">%s</th><th width="20%%">%s</th><th colspan="5" width="0">%s</th></tr></thead>',
     __( 'Name', 'proud-teaser' ),
     $dateLabel,
     empty($this->hide['content_available']) ? __( 'Content', 'proud-teaser' ) : ''
