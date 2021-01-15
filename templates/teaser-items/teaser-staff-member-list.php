0b3d3fcd5dc66cd24b171dff67e90e90df5d55df
Jeff Lyon <jeff@albatrossdigital.com>
2021-01-14 21:09:37 -0800

    Staff Member display: Add Title fields, hide labels if fields are empty https://github.com/proudcity/wp-proudcity/issues/1793

--- templates/teaser-items/teaser-staff-member-list.php
+++ templates/teaser-items/teaser-staff-member-list.php
@@ -1,16 +1,25 @@
 
 <div class="entry-content row">
-
 <div class="col-md-4 padding-md-left pull-md-right">
     <p><?php the_post_thumbnail( array( 300, 300 ) ); ?></p>
+    <?php if (!empty($meta['_staff_member_title'][0])): ?>
+    <p>
+        <strong>Title</strong><br/>
+        <?php echo $meta['_staff_member_title'][0]; ?>
+    </p>
+    <?php endif; ?>
+    <?php if (!empty($meta['_staff_member_phone'][0])): ?>
     <p>
         <strong>Phone</strong><br/>
         <?php echo $meta['_staff_member_phone'][0]; ?>
     </p>
+    <?php endif; ?>
+    <?php if (!empty($meta['_staff_member_email'][0])): ?>
     <p>
         <strong>Email</strong><br/>
         <a href="mailto:<?php echo $meta['_staff_member_email'][0]; ?>"><?php echo $meta['_staff_member_email'][0]; ?> </a>
     </p>
+    <?php endif; ?>
 </div>
     <div class="col-md-8">
         <?php the_title( sprintf( '<h3 class="h4 entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
