54fc0ed1398f6f6ae8e387782b8dd30469c84730
Curtis McHale <curtis@curtismchale.ca>
2022-03-02 09:23:48 -0800

    removing the extra header and tweaking the CSS to look appropriate

--- templates/page-header-breadcrumb.php
+++ templates/page-header-breadcrumb.php
@@ -6,6 +6,6 @@
     <?php if ( empty( $hide_mobile_menu ) ): ?>
     <a id="offcanvas-toggle" href="#" class="btn btn-primary visible-xs pull-right"><i class="fa fa-bars"></i></a>
     <?php endif; ?>
-    <h2><?php echo Wrapper\parent_title(); ?></h2>
+    <!--<h2><?php echo Wrapper\parent_title(); ?></h2>-->
 </div>
 <?php ProudBreadcrumb::print_breadcrumb() ?>
