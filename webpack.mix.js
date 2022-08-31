823ee69e0468b1d03d16d413c687f23e7c9383ef
Curtis McHale <curtis@curtismchale.ca>
2022-08-31 09:51:53 -0700

    getting our paths loaded

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,8 +4,24 @@ let mix = require('laravel-mix');
 
 const{ exec } = require('child_process');
 
-mix.before(()=>{
-    exec('git clone git@github.com:proudcity/proudcity-patterns.git bower_components/proudcity-patterns');
+mix.sass('assets/styles/proud.scss','dist/styles', {
+    sassOptions:{
+        outputStyle: "compressed",
+        includePaths: [
+            'bower_components/bootstrap-sass-official/assets/stylesheets',
+            'bower_components/bourbon/dist',
+            'bower_components/proudcity-patterns/app'
+        ],
+    }
+})
+    .sass('assets/styles/proud-vendor.scss', 'dist/styles', {
+        sassOptions:{
+            outputStyle: "compressed",
+            includePaths: [
+                'bower_components/bootstrap-sass-official/assets/stylesheets',
+                'bower_components/bourbon/dist',
+                'bower_components/proudcity-patterns/app'
+            ],
+        }
 });
-
-mix.sass('assets/styles/proud.scss','dist/styles');
+// add sourcemaps
