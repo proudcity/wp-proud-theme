8f564d96c385adea4e7e20ba853a81a22db671c1
Curtis McHale <curtis@curtismchale.ca>
2022-09-08 09:06:20 -0700

    cleaning up whitespace

--- templates/teaser-items/teaser-question-accordion.php
+++ templates/teaser-items/teaser-question-accordion.php
@@ -2,13 +2,13 @@
   <div class="panel-heading">
     <h4 class="panel-title">
       <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo the_id(); ?>">
-        <?php echo the_title(); ?> 
+        <?php echo the_title(); ?>
       </a>
     </h4>
   </div>
   <div id="collapse<?php echo the_id(); ?>" class="panel-collapse collapse">
     <div class="panel-body">
-      <?php echo the_content(); ?> 
+      <?php echo the_content(); ?>
     </div>
   </div>
 </div>
