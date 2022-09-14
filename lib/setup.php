80c5e7b50b580afa51e93df21de92470aa5f0cba
Curtis McHale <curtis@curtismchale.ca>
2022-09-14 10:24:44 -0700

    stopped loading minified versions of scripts

--- lib/setup.php
+++ lib/setup.php
@@ -163,18 +163,6 @@ function assets() {
     // Icons
     proud_theme_add_gov_icon_styles();
 
-  if ( 'production' === wp_get_environment_type() ){
-    // enqueue minified in production
-    // Add moderizer support
-    wp_enqueue_script( 'proud/js/modernizr',
-      Assets\asset_path( 'scripts/modernizr.min.js' ),
-      array()
-    );
-
-    wp_enqueue_script( 'proud/js', Assets\asset_path( 'scripts/main.min.js' ), ['jquery'], null, true );
-    wp_enqueue_script( 'proud/js', Assets\asset_path( 'scripts/bootstrap.min.js' ), ['jquery'], null, true );
-  } else {
-
     // Add moderizer support
     wp_enqueue_script( 'proud/js/modernizr',
       Assets\asset_path( 'scripts/modernizr.js' ),
@@ -183,7 +171,7 @@ function assets() {
 
     wp_enqueue_script( 'proud/js', Assets\asset_path( 'scripts/main.js' ), ['jquery'], null, true );
     wp_enqueue_script( 'bootstrap', Assets\asset_path( 'scripts/bootstrap.js' ), ['jquery'], null, true );
-  }
+
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100 );
 
