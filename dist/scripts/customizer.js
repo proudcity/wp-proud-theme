7e1a1f82aaeebb1bfa265e635a568387b6d83804
Curtis McHale <curtis@curtismchale.ca>
2022-09-20 14:18:06 -0700

    updating the mix build

--- dist/scripts/customizer.js
+++ dist/scripts/customizer.js
@@ -22,9 +22,12 @@
 /*!**********************************!*\
   !*** ./assets/styles/proud.scss ***!
   \**********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n6 │ @import \"../../../bower_components/proudcity-patterns/app/pattern-scss/local-variables\"\n  │         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n  ╵\n  assets/styles/loaders/_load-variables.scss 6:9  @import\n  assets/styles/loaders/_load-local.scss 9:9      @import\n  assets/styles/proud.scss 4:9                    root stylesheet\n    at processResult (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/webpack/lib/NormalModule.js:758:19)\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/webpack/lib/NormalModule.js:860:5\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at context.callback (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at Object.loader (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/sass-loader/dist/index.js:69:5)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -32,9 +35,12 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 /*!*****************************************!*\
   !*** ./assets/styles/proud-vendor.scss ***!
   \*****************************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n6 │ @import \"../../../bower_components/proudcity-patterns/app/pattern-scss/local-variables\"\n  │         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n  ╵\n  assets/styles/loaders/_load-variables.scss 6:9  @import\n  assets/styles/loaders/_load-vendor.scss 9:9     @import\n  assets/styles/proud-vendor.scss 9:9             root stylesheet\n    at processResult (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/webpack/lib/NormalModule.js:758:19)\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/webpack/lib/NormalModule.js:860:5\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at context.callback (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at Object.loader (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/sass-loader/dist/index.js:69:5)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -42,9 +48,12 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 /*!***********************************!*\
   !*** ./assets/styles/editor.scss ***!
   \***********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n6 │ @import \"../../../bower_components/proudcity-patterns/app/pattern-scss/local-variables\"\n  │         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n  ╵\n  assets/styles/loaders/_load-variables.scss 6:9  @import\n  assets/styles/loaders/_load-vendor.scss 9:9     @import\n  assets/styles/editor.scss 7:9                   root stylesheet\n    at processResult (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/webpack/lib/NormalModule.js:758:19)\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/webpack/lib/NormalModule.js:860:5\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at context.callback (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at Object.loader (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/sass-loader/dist/index.js:69:5)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -52,24 +61,161 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 /*!******************************************!*\
   !*** ./assets/styles/ie9-and-below.scss ***!
   \******************************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n6 │ @import \"../../../bower_components/proudcity-patterns/app/pattern-scss/local-variables\"\n  │         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n  ╵\n  assets/styles/loaders/_load-variables.scss 6:9  @import\n  assets/styles/loaders/_load-local.scss 9:9      @import\n  assets/styles/ie9-and-below.scss 4:9            root stylesheet\n    at processResult (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/webpack/lib/NormalModule.js:758:19)\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/webpack/lib/NormalModule.js:860:5\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at context.callback (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at Object.loader (/Users/curtismchale/Local Sites/proudtest/app/public/wp-content/themes/wp-proud-theme/node_modules/sass-loader/dist/index.js:69:5)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/dist/scripts/customizer": 0,
+/******/ 			"dist/styles/ie9-and-below": 0,
+/******/ 			"dist/styles/editor": 0,
+/******/ 			"dist/styles/proud-vendor": 0,
+/******/ 			"dist/styles/proud": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunkwp_proud_theme"] = self["webpackChunkwp_proud_theme"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	__webpack_modules__["./assets/scripts/customizer.js"]();
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	__webpack_modules__["./assets/styles/proud.scss"]();
-/******/ 	__webpack_modules__["./assets/styles/proud-vendor.scss"]();
-/******/ 	__webpack_modules__["./assets/styles/editor.scss"]();
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./assets/styles/ie9-and-below.scss"]();
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["dist/styles/ie9-and-below","dist/styles/editor","dist/styles/proud-vendor","dist/styles/proud"], () => (__webpack_require__("./assets/scripts/customizer.js")))
+/******/ 	__webpack_require__.O(undefined, ["dist/styles/ie9-and-below","dist/styles/editor","dist/styles/proud-vendor","dist/styles/proud"], () => (__webpack_require__("./assets/styles/proud.scss")))
+/******/ 	__webpack_require__.O(undefined, ["dist/styles/ie9-and-below","dist/styles/editor","dist/styles/proud-vendor","dist/styles/proud"], () => (__webpack_require__("./assets/styles/proud-vendor.scss")))
+/******/ 	__webpack_require__.O(undefined, ["dist/styles/ie9-and-below","dist/styles/editor","dist/styles/proud-vendor","dist/styles/proud"], () => (__webpack_require__("./assets/styles/editor.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["dist/styles/ie9-and-below","dist/styles/editor","dist/styles/proud-vendor","dist/styles/proud"], () => (__webpack_require__("./assets/styles/ie9-and-below.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
 ;
