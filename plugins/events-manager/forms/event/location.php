15f8f20651946f0ca76b4e51755eaecabf03e289
Curtis McHale <curtis@curtismchale.ca>
2022-10-25 10:53:22 -0700

    saving killed trailing whitespace

--- plugins/events-manager/forms/event/location.php
+++ plugins/events-manager/forms/event/location.php
@@ -26,21 +26,21 @@ $required = apply_filters('em_required_html','<i>*</i>');
 		<input id='location-latitude' class="form-control" name='location_latitude' type='text' value='<?php echo $EM_Event->get_location()->location_latitude; ?>' size='15' />
 		<input id='location-longitude' class="form-control" name='location_longitude' type='text' value='<?php echo $EM_Event->get_location()->location_longitude; ?>' size='15' />
 	</div>
-	<?php if( get_option('dbem_use_select_for_locations') || !$EM_Event->can_manage('edit_locations','edit_others_locations') ) : ?> 
+	<?php if( get_option('dbem_use_select_for_locations') || !$EM_Event->can_manage('edit_locations','edit_others_locations') ) : ?>
 	<table class="em-location-data">
 		<tr class="em-location-data-select">
 			<th><?php _e('Location:','events-manager') ?> </th>
-			<td> 
-				<select name="location_id" id='location-select-id' size="1">  
+			<td>
+				<select name="location_id" id='location-select-id' size="1">
 					<?php if(!get_option('dbem_require_location',true)): ?><option value="0"><?php _e('No Location','events-manager'); ?></option><?php endif; ?>
-					<?php 
+					<?php
 					$ddm_args = array('blog'=>false, 'private'=>$EM_Event->can_manage('read_private_locations'));
 					$ddm_args['owner'] = (is_user_logged_in() && !current_user_can('read_others_locations')) ? get_current_user_id() : false;
 					$locations = EM_Locations::get($ddm_args);
 					$selected_location = !empty($EM_Event->location_id) || !empty($EM_Event->event_id) ? $EM_Event->location_id:get_option('dbem_default_location');
 					foreach($locations as $EM_Location) {
 						$selected = ($selected_location == $EM_Location->location_id) ? "selected='selected' " : '';
-				   		?>          
+				   		?>
 				    	<option value="<?php echo $EM_Location->location_id ?>" title="<?php echo "{$EM_Location->location_latitude},{$EM_Location->location_longitude}" ?>" <?php echo $selected ?>><?php echo $EM_Location->location_name; ?></option>
 				    	<?php
 					}
@@ -51,7 +51,7 @@ $required = apply_filters('em_required_html','<i>*</i>');
 	</table>
 	<?php else : ?>
 	<div class="form-horizontal">
-		<?php 
+		<?php
 			global $EM_Location;
 			if( $EM_Event->location_id !== 0 ){
 				$EM_Location = $EM_Event->get_location();
@@ -65,7 +65,7 @@ $required = apply_filters('em_required_html','<i>*</i>');
 			<label class="col-sm-3 control-label"><?php _e ( 'Location Name:', 'events-manager')?></label>
 			<div class="col-sm-6">
 				<input id='location-id' name='location_id' type='hidden' value='<?php echo $EM_Location->location_id; ?>' size='15' />
-				<input id="location-name" class="form-control" type="text" name="location_name" value="<?php echo esc_attr($EM_Location->location_name, ENT_QUOTES); ?>" /><?php echo $required; ?>													
+				<input id="location-name" class="form-control" type="text" name="location_name" value="<?php echo esc_attr($EM_Location->location_name, ENT_QUOTES); ?>" /><?php echo $required; ?>
 				<br />
 				<em id="em-location-search-tip"><?php _e( 'Create a location or start typing to search a previously created location.', 'events-manager')?></em>
 				<em id="em-location-reset" style="display:none;"><?php _e('You cannot edit saved locations here.', 'events-manager'); ?> <a href="#"><?php _e('Reset this form to create a location or search again.', 'events-manager')?></a></em>
