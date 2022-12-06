741f08133b25218b2a3b2a29d62ed073079ef57e
Curtis McHale <curtis@curtismchale.ca>
2022-12-06 13:17:56 -0800

    - fixed the single issue template
    First, we removed the `link` option as the default. If you choose Custom
    Text in the post then it will also try to send you to the link. When the
    link isn't set (because you can't set it when you have picked custom
    text) it will simply cycle you on the page with a blank URL value. Now
    you only get a the link to an external resource as a redirect when you
    choose external link.
    
    Second, revamped the template in light of security. Revamping the
    escaping and overall structure of the vars.

--- templates/content-single-issue.php
+++ templates/content-single-issue.php
@@ -1,6 +1,5 @@
 <?php
-$post_id = get_the_ID();
-$type = get_post_meta( $post_id, 'issue_category_type', true );
+$type = get_post_meta( absint( get_the_ID() ), 'issue_category_type', true );
 ?>
 
 <article <?php post_class(); ?>><!-- template-file: templates/content-single-issue.php -->
@@ -10,14 +9,14 @@ $type = get_post_meta( $post_id, 'issue_category_type', true );
   <div class="entry-content row" id="entry-content">
     <div class="col-md-9">
       <?php the_content(); ?>
-      <?php if ($type == 'iframe'): ?>
-        <iframe src="<?php echo get_post_meta( $post_id, 'iframe', true ); ?>" style="border:none;height:1500px;width:100%"></iframe>
-      <?php elseif($type == 'form'): ?>
-        <?php gravity_form( get_post_meta( $post_id, 'form', true ), false, false ); ?>
-      <?php else: ?>
+      <?php if ( esc_attr( $type ) === 'iframe'): ?>
+        <iframe src="<?php echo esc_url( get_post_meta( absint( get_the_ID() ), 'iframe', true ) ); ?>" style="border:none;height:1500px;width:100%"></iframe>
+      <?php elseif( esc_attr( $type ) === 'form'): ?>
+        <?php gravity_form( get_post_meta( absint( get_the_ID() ), 'form', true ), false, false ); ?>
+      <?php elseif( esc_attr( $type ) === 'link' ): ?>
         <script type="text/javascript">
           jQuery('#entry-content').html('<div class="text-center"><i aria-hidden="true" class="fa fa-spinner fa-pulse fa-4x"></i></div>');
-          window.location = '<?php echo get_post_meta( $post_id, 'url', true ) ?>';
+          window.location = '<?php echo esc_url( get_post_meta( absint( get_the_ID() ), 'url', true ) ); ?>';
         </script>
       <?php endif ?>
     </div>
