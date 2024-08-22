c3c38fedf91be0627eb25369103c7e982d4e2787
curtismchale <curtis@curtismchale.c>
2024-08-22 08:57:57 -0700

    fixing the accessibilyt and styles for meeting lists

--- templates/teaser-items/teaser-meeting-list.php
+++ templates/teaser-items/teaser-meeting-list.php
@@ -1,39 +1,41 @@
-<div <?php post_class( "teaser" ); ?>><!-- template-file: teaser-meeting-list.php -->
+<div <?php post_class("teaser"); ?>><!-- template-file: teaser-meeting-list.php -->
   <?php
     // Date formats (same as Events)
     $datebox_format = 'M \<\s\p\a\n \c\l\a\s\s=\"\d\a\t\e\-\b\i\g\"\>j\<\/\s\p\a\n\> Y';
-    $atc_format = 'Y-m-d H:i:s';
-    $time_format = 'g:i a';
+$atc_format = 'Y-m-d H:i:s';
+$time_format = 'g:i a';
 
-  //$datetime = new DateTime(get_post_meta($id, 'datetime', true));
-    $is_upcoming = $datetime > new DateTime();
-  ?>
+//$datetime = new DateTime(get_post_meta($id, 'datetime', true));
+$is_upcoming = $datetime > new DateTime();
+?>
   <div class="row">
+    <?php the_title(sprintf('<h3 class="h4 entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h3>'); ?>
     <div class="col-xs-3 col-md-2">
       <div class="date-box"><?php echo date_format($datetime, $datebox_format) ?></div>
     </div>
     <div class="col-xs-9 col-md-10">
-      <?php the_title( sprintf( '<h3 class="h4 entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
-        <h5 class="margin-top-none"><span class="start-time"><?php echo date_format($datetime, $time_format) ?></span>
-        <?php if( !empty( $location_name ) ) :?>
-          <i aria-hidden="true" class="fa fa-caret-right icon-even-width text-center"></i> <span class="location"><?php echo $location_name ?></span>
-        <?php endif; ?></h5>
+        <p class="margin-top-none"><time class="start-time"><?php echo date_format($datetime, $time_format) ?></time>
+        <?php if(!empty($location_name)) :?>
+          <i aria-hidden="true" class="fa fa-caret-right icon-even-width text-center"></i> <span class="location"><?php echo esc_attr($location_name); ?></span>
+        <?php endif; ?></p>
       </ul>
       <?php if($is_upcoming): ?>
 
-        <ul class="list-inline">
+      <ul class="list-inline">
+        <!--
         <li>
           <?php
-            // builds out or HTML, JSON and such for the add to calendar button
-            Proud\Theme\Extras\get_atcb_button( $post, $location, $datetime );
+          // builds out or HTML, JSON and such for the add to calendar button
+          Proud\Theme\Extras\get_atcb_button($post, $location, $datetime);
           ?>
         </li>
-        <?php if ( !empty($location) ) { ?>
+        -->
+        <?php if (!empty($location)) { ?>
           <li><a href="https://maps.google.com?daddr=<?php echo urlencode($location) ?>" target="_blank" class="btn btn-xs btn-default  btn-block"><i aria-hidden="true" class="fa fa-map-marker"></i> Directions</a></li>
-        <?php } //endif ?>
+        <?php } //endif?>
         </ul>
       <?php endif; ?>
-      <?php //do_action( 'teaser_search_matching', $post ); ?>
+      <?php //do_action( 'teaser_search_matching', $post );?>
     </div>
   </div>
 </div>
