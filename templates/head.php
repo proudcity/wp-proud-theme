99b072f8b4a0a3a5ea3fe5dc519b5f80bbdf06a3
Curtis McHale <curtis@curtismchale.ca>
2025-02-27 13:41:16 -0800

    stop mime sniffing

--- templates/head.php
+++ templates/head.php
@@ -2,27 +2,28 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+  <meta http-equiv="X-Content-Type-Options" content="nosniff">
   <?php
 
   wp_head();
-//
-//  function print_filters_for( $hook = '' ) {
-//	  global $wp_filter;
-//	  if( empty( $hook ) || !isset( $wp_filter[$hook] ) )
-//		  return;
-//
-//	  print '<pre>';
-//	  print_r( $wp_filter[$hook] );
-//	  print '</pre>';
-//  }
-//
-//
-//
-?>
+  //
+  //  function print_filters_for( $hook = '' ) {
+  //	  global $wp_filter;
+  //	  if( empty( $hook ) || !isset( $wp_filter[$hook] ) )
+  //		  return;
+  //
+  //	  print '<pre>';
+  //	  print_r( $wp_filter[$hook] );
+  //	  print '</pre>';
+  //  }
+  //
+  //
+  //
+  ?>
 
   <?php
-  if ( ! has_post_thumbnail() ){ ?>
-     <meta properyt="og:image" content="<?php echo esc_url( get_option( 'proud_default_social_image' ) ); ?>" />
-  <?php } // has_post_thumnail ?>
+    if (! has_post_thumbnail()) { ?>
+     <meta properyt="og:image" content="<?php echo esc_url(get_option('proud_default_social_image')); ?>" />
+  <?php } // has_post_thumnail?>
 
 </head>
