5ca8eeb9b330f3af19f4fb790098c1ed48079861
Curtis McHale <curtis@curtismchale.ca>
2025-03-26 10:05:53 -0700

    indentation

--- base.php
+++ base.php
@@ -2,18 +2,19 @@
 
 use Proud\Theme\Setup;
 use Proud\Theme\Wrapper;
+
 global $proudcore;
 
 if (!$proudcore) {
-  echo "<html><body><h1>You need ProudCore to use this theme</h1></body></html>";
-  exit;
+    echo "<html><body><h1>You need ProudCore to use this theme</h1></body></html>";
+    exit;
 }
 
 ?>
 
 <!doctype html>
 <html <?php language_attributes(); ?>>
-  <?php get_template_part( 'templates/head' ); ?>
+  <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
     <!--[if IE]>
       <div class="alert alert-warning">
@@ -22,23 +23,23 @@ if (!$proudcore) {
     <![endif]-->
     <?php
       do_action('get_header');
-    ?>
+?>
     <?php echo Wrapper\alert_bar(); ?>
     <div class="wrap <?php echo Wrapper\container_class(); ?>" role="document">
 
-      <?php if ( $proudcore::$layout->page_parent_info( 'title' ) ) : ?>
-          <?php get_template_part( 'templates/page-header-breadcrumb' ); ?>
+      <?php if ($proudcore::$layout->page_parent_info('title')) : ?>
+          <?php get_template_part('templates/page-header-breadcrumb'); ?>
       <?php endif; ?>
 
       <div id="content-main" class="content row">
 
-        <?php if ( $proudcore::$layout->page_parent_info( 'noagency' ) ) : ?>
+        <?php if ($proudcore::$layout->page_parent_info('noagency')) : ?>
           <aside class="sidebar">
             <?php include Wrapper\sidebar_path(); ?>
           </aside><!-- /.sidebar -->
         <?php endif; ?>
 
-        <?php if ( $proudcore::$layout->page_parent_info( 'agency') ) : ?>
+        <?php if ($proudcore::$layout->page_parent_info('agency')) : ?>
           <aside class="sidebar">
             <?php include Wrapper\sidebar_agency_path(); ?>
           </aside><!-- /.sidebar -->
@@ -51,12 +52,15 @@ if (!$proudcore) {
       </div><!-- /.content -->
     </div><!-- /.wrap -->
     <?php
-      do_action('get_footer');
-      get_template_part('templates/footer-actions');
-      get_template_part('templates/footer');
-      do_action('proud_footer');
-      do_action('proud_settings');
-      wp_footer();
-    ?>
+
+    do_action('get_footer');
+
+get_template_part('templates/footer-actions');
+get_template_part('templates/footer');
+do_action('proud_footer');
+do_action('proud_settings');
+wp_footer();
+
+?>
   </body>
 </html>
