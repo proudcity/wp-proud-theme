315af044568e3d521fcf270cdeb0fd93540e8bfa
Curtis McHale <curtis@curtismchale.ca>
2026-02-24 13:24:24 -0800

    adding text inside the menu for screenreaders

--- templates/page-header-breadcrumb.php
+++ templates/page-header-breadcrumb.php
@@ -1,10 +1,11 @@
 <?php
-    use Proud\Theme\Wrapper;
-    use Proud\Core\ProudBreadcrumb;
+
+use Proud\Theme\Wrapper;
+use Proud\Core\ProudBreadcrumb;
 ?>
 <div class="page-header"><!-- template-file: templates/page-header-breadcrumb.php -->
-    <?php if ( empty( $hide_mobile_menu ) ): ?>
-    <a id="offcanvas-toggle" href="#" class="btn btn-primary visible-xs pull-right"><i aria-hidden="true" class="fa fa-bars"></i></a>
+    <?php if (empty($hide_mobile_menu)): ?>
+        <a id="offcanvas-toggle" href="#" class="btn btn-primary visible-xs pull-right"><i aria-hidden="true" class="fa fa-bars"></i><span class="sr-only">Menu</span></a>
     <?php endif; ?>
     <!--<h2><?php echo Wrapper\parent_title(); ?></h2>-->
 </div>
