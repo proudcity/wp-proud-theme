5572c9b19ba6537565e70e34f3de3d41234140d7
Curtis McHale <curtis@curtismchale.ca>
2026-03-18 09:16:15 -0700

    adding custom template for new proud topic

---
+++ templates/content-single-proud-topic.php
@@ -0,0 +1,18 @@
+<?php use Proud\Theme\Titles; ?>
+<!-- template-file: templates/content-single-proud-topic.php -->
+
+<article <?php post_class(); ?>>
+  <?php if( !Titles\titleHidden() ) : ?>
+  <header>
+    <h1 class="entry-title"><?php the_title(); ?></h1>
+  </header>
+  <?php endif; ?>
+  <div class="entry-content">
+      <?php if( has_post_thumbnail() && !Titles\titleHidden() ): ?>
+      <p>
+          <?php the_post_thumbnail(); ?>
+      </p>
+      <?php endif; ?>
+    <?php the_content(); ?>
+  </div>
+</article>
